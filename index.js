bf614389fd045db2827a3b03c46dd1a01ca76fdd
GolovchikTats <tatsiana.golovchik.vrp@gmail.com>
2018-08-27 09:38:21 +0300

    plus passport.js

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
-// hbs:
-const hbs = require('hbs');
-
-const express = require("express");
+const express = require('express');
 const app = express();
-// app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+const passport   = require('passport');
+const session    = require('express-session');
+const bodyParser = require('body-parser');
+const env = require('dotenv').load();
+const exphbs = require('express-handlebars');
+
+app.use('/static', express.static('static'));
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); 
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+app.get('/', function(req, res) {
+    res.send('Welcome to Passport with Sequelize');
+});
+ 
+app.listen(5000, function(err) {
+    if (!err)
+        console.log("Site is live");
+    else console.log(err)
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+//Models (импорт моделей):
+const models = require("./app/models");
+
+//Routes 
+const authRoute = require('./routes/auth.js')(app, passport);
+
+//load passport strategies
+require('./app/config/passport/passport.js')(passport, models.user);
+ 
+//Sync Database (синхронизация Sequelize):
+models.sequelize.sync().then(function() {
+    console.log('Nice! Database looks fine');
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!");
 });
+
+//For Handlebars:
+app.set('views', './views');
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+
+
