42eabd97dd31c90dadc991189def6564733700fa
TatsianaHal <tripwithtanya@gmail.com>
2018-10-09 14:41:31 +0300

    transfer 'user' file from 'app' to 'db' folder #19

--- db/models/user.js
+++ db/models/user.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING,
       notEmpty: true,
     },
-    username: {
+    fullname: {
       type: Sequelize.TEXT,
     },
     about: {
