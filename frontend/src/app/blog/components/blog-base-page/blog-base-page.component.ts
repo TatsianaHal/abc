43f324ffbefea703d8bde3039be536f242ae353c
TatsianaHal <tripwithtanya@gmail.com>
2018-10-23 17:29:48 +0300

    add 'blog-base-page' component issues/#13

---
+++ frontend/src/app/blog/components/blog-base-page/blog-base-page.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../../services';
+
+@Component({
+  selector: 'app-blog-base-page',
+  templateUrl: './blog-base-page.component.html',
+  styleUrls: ['./blog-base-page.component.css']
+})
+export class BlogBasePageComponent implements OnInit {
+
+  constructor(private appServiceComponents: BlogService) {}
+  user = {
+    email: '',
+    password: ''
+  }
+
+  userSubmit() {
+    this.appServiceComponents.getAllUsers()
+                             .subscribe(res => {console.log(res)});
+  }
+
+  ngOnInit() {
+  }
+
+}
